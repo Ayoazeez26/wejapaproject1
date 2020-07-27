c67ff4a1800b0f74db10d3c8ea2bee0af1673f76
Abdulhakeem Abdulazeez <ayoazeez26@gmail.com>
2020-07-27 13:03:44 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Hakeem
+      name="Abdulhakeem"
+    />
+  </div>
+</template>
+
+<script>
+import Hakeem from './components/Hakeem.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Hakeem
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Hakeem.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ name }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'MyName',
+  props: {
+    name: String
+  }
+}
+</script>
+
+<style scoped>
+  h1 {
+    margin: 0 auto;
+    font-size: 60px;
+  }
+</style>
